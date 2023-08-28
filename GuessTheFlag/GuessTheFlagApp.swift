43bd0b6d5ba8eb04e77a302546f86ae54ad2e8d2
Chuck Belcher <cfbelcher@pateogroup.com>
2023-08-28 12:24:14 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Chuck Belcher on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
